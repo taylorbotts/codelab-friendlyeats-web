567ccd2f4c0fe981906649d7f5696699dd14e551
Taylor <taylorbotts@knights.ucf.edu>
2024-11-07 22:21:27 -0500

    remove old code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,6 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-88134","appId":"1:720554123854:web:41ed99bb924f0a81db991f","storageBucket":"friendlychat-88134.firebasestorage.app","apiKey":"AIzaSyDR3GlLG6f3vyplkHU0PLl7-arPEvvFkN4","authDomain":"friendlychat-88134.firebaseapp.com","messagingSenderId":"720554123854"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
